213feb72869636bc1a94cddaddcea367c9ee4ac4
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-04-08 19:07:04 +0000

    Removed scret key and set debug.

--- ms4_vintage_design/settings.py
+++ ms4_vintage_design/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#3jwx-$ax0v5!z4#bh)b_$qz1h#-^y-rsf=&0!0!tm&82)x4es'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vintage-design.herokuapp.com', 'localhost']
 
